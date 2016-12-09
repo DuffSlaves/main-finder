17478426196470a9523ea053788e784199ae76a8
Phantomical <phantom@lynches.ca>
2016-12-08 21:58:32 -0500

    Added commands for class file analysis

--- src/main/java/MainFinder/MainFinder.java
+++ src/main/java/MainFinder/MainFinder.java
@@ -7,11 +7,20 @@ import java.net.*;
 import org.apache.bcel.classfile.*;
 
 public class MainFinder {
+
+	public static void printHelp() {
+		System.err.println("Usage: find-main <command> <class-path>");
+		System.err.println("Commands:\n"
+				+ "    p  Print the package that the class is in.\n"
+				+ "    pp Print the package that the class is in as a unix path.\n"
+				+ "    m  Print all methods within the class.\n"
+				+ "    mm Print the main method of the class. If there is no main method return error.\n");		
+	}
 	public static void main(String[] args) {
 		if (args.length < 2)
 		{
 			System.err.println("Not enough arguments given.");
-			System.err.println("Usage: find-main <command> <class-path>");
+			printHelp();
 			System.exit(1);
 		}
 
@@ -21,9 +30,13 @@ public class MainFinder {
 		{
 			JavaClass jclass = new ClassParser(args[1]).parse();
 
-			if (args[0].equals("p"))
+			if (args[0].equals("pp"))
 			{
 				System.out.println(jclass.getPackageName().replaceAll("\\.", "//"));
+			}
+			else if (args[0].equals("pp"))
+			{
+				System.out.println(jclass.getPackageName());
 			}
 			else if (args[0].equals("m"))
 			{
@@ -33,16 +46,40 @@ public class MainFinder {
 					System.out.println(method.getSignature());
 				}				
 			}
-			else
+			else if (args[0].equals("mm"))
 			{
-				System.out.println("Class: " + jclass.getClassName());
-				System.out.println(jclass.getPackageName());
-				System.out.println("  Methods:");
+				ArrayList<Method> mains = new ArrayList<Method>();
+
 				for (Method method : jclass.getMethods())
 				{
-					System.out.println(method.getName());
-					System.out.println(method.getSignature());
+					if (method.getName().equals("main") &&
+							method.getSignature().equals("([Ljava/lang/String;)V"))
+					{
+						mains.add(method);
+					}
 				}
+				
+				if (mains.size() == 0)
+				{
+					System.err.println("Error: No main method found.");
+					System.exit(10);
+				}
+				else if (mains.size() > 1)
+				{
+					System.err.println("Error: Multiple main methods within the same class.");
+					System.exit(11);
+				}
+				
+				System.out.print(jclass.getPackageName() + '.');
+				System.out.print(jclass.getClassName() + '.');
+				System.out.print(mains.get(0).getName());
+				System.out.print(mains.get(0).getSignature());
+				System.out.println();
+			}
+			else
+			{
+				System.err.println("Error: Invalid command.");
+				System.exit(1);
 			}
 		}
 		catch (IOException e)
